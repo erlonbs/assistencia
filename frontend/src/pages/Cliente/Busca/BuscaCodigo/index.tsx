7fb4dc524aede53e39d8fa481594f64c973d302e
erlonecia@msn.com <erlonecia@msn.com>
2023-09-22 21:42:44 -0300

    Resolvendo conflitos

--- frontend/src/pages/Cliente/Busca/BuscaCodigo/index.tsx
+++ frontend/src/pages/Cliente/Busca/BuscaCodigo/index.tsx
@@ -7,11 +7,9 @@ import { BASE_URL } from 'utils/requests'
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
-
 function BuscaCodigo() {
+  let name = ''
 
-let name ='';
-  
   const [clienteId, setClienteId] = useState<string>()
 
   const [cliente, setCliente] = useState<Clientes>()
@@ -22,54 +20,31 @@ let name ='';
     })
   }, [clienteId])
 
-  const handleSubmit = (event: { preventDefault: () => void }) =>{
-    event.preventDefault();
-<<<<<<< HEAD
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault()
+
     alert(`código inserido do cliente: ${name}`)
 
     setClienteId(name)
-=======
-    setClienteId(name)
-    
->>>>>>> dd91e96603d911bd8c36063ce8dddc74f9c72656
   }
 
   return (
-
-    
     <body>
       <form onSubmit={handleSubmit}>
         <h2>Entre com o código do cliente:</h2>
 
         <div className="entrada">
-
-          
           <input
             type="text"
             name="codigo"
-<<<<<<< HEAD
             value={clienteId}
-            
-          
-            onChange={e => (name= e.target.value)}
-            
-=======
-            value={clienteId}            
-          
-            onChange={e => (name= e.target.value)}            
->>>>>>> dd91e96603d911bd8c36063ce8dddc74f9c72656
-                       
-          ></input >
-
+            onChange={e => (name = e.target.value)}
+          ></input>
         </div>
- 
-      
       </form>
-<<<<<<< HEAD
+
       <h2>O código do cliente é : {clienteId}</h2>
-=======
-  
->>>>>>> dd91e96603d911bd8c36063ce8dddc74f9c72656
+
       <div>
         <p className="dados">
           <h3>Dados do cliente:</h3>
--- frontend/src/pages/Cliente/Busca/index.tsx
+++ frontend/src/pages/Cliente/Busca/index.tsx
@@ -1,25 +1,8 @@
-import { Clientes } from 'types/cliente'
-import { BASE_URL } from 'utils/requests'
-<<<<<<< HEAD
-
 import './styles.css'
-
-
 import { Link } from 'react-router-dom'
 
-
-=======
-import './styles.css'
-
-import { Link } from 'react-router-dom'
-
->>>>>>> dd91e96603d911bd8c36063ce8dddc74f9c72656
 function Busca() {
- 
-
   return (
-   
-      
     <body>
       <div className="busca">
         <label htmlFor="bt">"buscar todos"</label>
@@ -37,15 +20,13 @@ function Busca() {
           <a href="/Cliente/Busca/BuscaCodigo">
             <input type="button" value="Buscar" />
           </a>
-          
         </form>
 
         <Link className="voltar" to="/Cliente/1">
-        <div>
-          <button>voltar</button>
-        </div>
-      
-      </Link>
+          <div>
+            <button>voltar</button>
+          </div>
+        </Link>
       </div>
     </body>
   )
