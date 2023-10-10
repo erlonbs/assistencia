ba472ad795134cf06b4bae8c3e88a9d63c351e30
erlonecia@msn.com <erlonecia@msn.com>
2023-10-09 23:18:30 -0300

    Adicionado icones excluir e editar na busca por codigo

--- frontend/src/pages/Cliente/Busca/BuscaCodigo/index.tsx
+++ frontend/src/pages/Cliente/Busca/BuscaCodigo/index.tsx
@@ -5,81 +5,93 @@ import { Clientes } from 'types/cliente'
 import { BASE_URL } from 'utils/requests'
 
 import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import Atualizar from 'pages/Cliente/Atualizar'
-import { link } from 'fs'
+import { Link } from 'react-router-dom';
+
+import editarImagem from 'assets/icon/editar.png';
+
+import excluirImagem from 'assets/icon/excluir.png';
 
 function BuscaCodigo() {
-  
-
   const [clienteId, setClienteId] = useState<string>()
 
   const [cliente, setCliente] = useState<Clientes>()
 
-  const[mensagem, setMensagem] = useState<string>();
-
+  const [mensagem, setMensagem] = useState<string>()
 
   useEffect(() => {
     axios.get(`${BASE_URL}/clientes/${clienteId}`).then(response => {
       setCliente(response.data)
     })
   }, [clienteId])
- 
-  const handleSubmit = (event:  React.FormEvent) => {
+
+  const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault()
-   
-    if (cliente ){
+
+    if (cliente) {
       setMensagem(`Verifique abaixo os dados de : ${cliente.name}`)
-    }
-    else {
+    } else {
       setMensagem(' Cliente inexistente')
-    }     
-    
-  };
+    }
+  }
 
   return (
     <div>
-      <form className='formulario' onSubmit={handleSubmit}>
+      <form className="formulario" onSubmit={handleSubmit}>
         <h2>Entre com o código do cliente:</h2>
 
         <div className="entrada">
           <input
             type="text"
-          name="codigo"
+            name="codigo"
             value={clienteId}
-            onChange={e => (setClienteId(e.target.value))}
-         
+            onChange={e => setClienteId(e.target.value)}
           ></input>
         </div>
-        <button className='botaoEnviar' type='submit'>Consultar</button>
+        <button className="botaoEnviar" type="submit">
+          Consultar
+        </button>
       </form>
 
-      {mensagem && (
-        <div className="mensagem">{mensagem}</div>
-      )}
-             
-<table className='tabela'>
-        <thead >
-          <tr className='coluna' >
-            <th className='cabecalho'>Código</th>
-            <th className='cabecalho'>Nome</th>
-            <th className='cabecalho'>Endereço</th>
-            <th className='cabecalho'>Telefone</th>
-            <th className='cabecalho'>CPF</th>
-            <th className='cabecalho'>Editar</th>
-            <th className='cabecalho'>Excluir</th>
+      {mensagem && <div className="mensagem">{mensagem}</div>}
+
+      <table className="tabela">
+        <thead>
+          <tr className="coluna">
+            <th className="cabecalho">Código</th>
+            <th className="cabecalho">Nome</th>
+            <th className="cabecalho">Endereço</th>
+            <th className="cabecalho">Telefone</th>
+            <th className="cabecalho">CPF</th>
+            <th className="cabecalho">Editar</th>
+            <th className="cabecalho">Excluir</th>
           </tr>
         </thead>
-        <tbody className='conteudo'>
+        <tbody className="conteudo">
           {
-            <tr className='coluna'  key={cliente?.id}>
-              <td className='celula'>{cliente?.id}</td>
-              <td className='celula'>{cliente?.name}</td>
-              <td className='celula'>{cliente?.address}</td>
-              <td className='celula'>{cliente?.telephone}</td>
-              <td className='celula'>{cliente?.cpf}</td>
-              <td className='celula'><a href="/Cliente/Atualizar"><img className='editar' src="frontend/assets/icon/editar.png" alt="icone editar" /></a></td>
-              <td className='celula'><a href="/Cliente/Excluir"><img className='editar' src="frontend/assets/icon/excluir.png" alt="icone excluir" /></a></td>
+            <tr className="coluna" key={cliente?.id}>
+              <td className="celula">{cliente?.id}</td>
+              <td className="celula">{cliente?.name}</td>
+              <td className="celula">{cliente?.address}</td>
+              <td className="celula">{cliente?.telephone}</td>
+              <td className="celula">{cliente?.cpf}</td>
+              <td className="celula">
+                <a href="/Cliente/Atualizar">
+                  <img
+                    className="editar"
+                    src={editarImagem}
+                    alt="icone editar"
+                  />
+                </a>
+              </td>
+              <td className="celula">
+                <a href="/Cliente/Excluir">
+                  <img
+                    className="excluir"
+                    src={excluirImagem}
+                    alt="icone excluir"
+                  />
+                </a>
+              </td>
             </tr>
           }
         </tbody>
